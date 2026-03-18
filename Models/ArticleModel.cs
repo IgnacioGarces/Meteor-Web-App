e075ab21c516447b65269a9cd35327795858fe22
Ignacio <ignaciogarces96@gmail.com>
2026-03-18 14:55:02 -0300

    Unhandled error flag remove. Starting to mobile first

--- Models/ArticleModel.cs
+++ Models/ArticleModel.cs
@@ -3,10 +3,10 @@
     public class ArticleModel
     {
         public DateTime Date { get; set; }
-        public string ImageUrl { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public string StoreUrl { get; set; }
+        public required string ImageUrl { get; set; }
+        public required string Title { get; set; }
+        public required string Content { get; set; }
+        public required string StoreUrl { get; set; }
         public int Id { get; internal set; }
     }
 }
